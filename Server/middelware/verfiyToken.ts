226db993795ab63c417cccc3ed62d763f8aa0e97
mohamed ahmed <ma614351@gmail.com>
2023-11-16 17:22:44 +0100

    first commit

---
+++ Server/middelware/verfiyToken.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import user from "../models/user"
+
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+const verifyToken = (req : Request, res : Response , next : NextFunction ) => {
+  const token = req.headers["authorization"];
+  console.log(req.headers);
+  if (!token) {
+    return res.status(403).send("Access Denied");
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_SECRET);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+module.exports = verifyToken;
