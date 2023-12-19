70b5facd10b8edc79a49cd12c958eff07f4aedb2
mohamed ahmed <ma614351@gmail.com>
2023-12-19 16:21:12 +0100

    navbar

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,14 +1,24 @@
-import { Flex , Box  } from '@chakra-ui/react'
+import { Flex, Text , Image } from "@chakra-ui/react";
 
 function Navbar() {
   return (
-    <Flex>
-        <Box as="div" ></Box>
+    <Flex bg="#F3F3FD" >
+      <Image src="/logo.svg" width="40px" height="40px" ml="40px" mt="10px" />
+      <Text
+        fontFamily="Satoshi"
+        color="#6F3AFA "
+        fontSize="20px"
+        ml="10px"
+        fontStyle="normal"
+        fontWeight="980"
+        width="224px"
+        height="58px"
+      >
+        HealthyMe
+      </Text>
+      <Image src="./logo2.svg" width="30px"  height="30px"  ml="0px" />
     </Flex>
-  )
+  );
 }
 
-export default Navbar
-
-
-
+export default Navbar;
